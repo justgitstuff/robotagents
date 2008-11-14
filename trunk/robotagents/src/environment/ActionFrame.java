1b08c204a844ea237c70eef8edcd112472a670c4
macku30 <macku30@9c259208-ad8a-11dd-8830-69e4bb380a4a>
2008-11-14 11:19:23 +0000

    Env GUI with full functionality, still not connected to EnvironmentAgent

--- trunk/robotagents/src/environment/ActionFrame.java
+++ trunk/robotagents/src/environment/ActionFrame.java
@@ -3,13 +3,18 @@ package environment;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 import java.awt.geom.Ellipse2D;
+import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.lang.Math;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.text.*;
 
 import utils.EnvObject;
 
@@ -21,18 +26,23 @@ import utils.EnvObject;
 public class ActionFrame extends JFrame
 {
    private EnvPanel panel;
-   private JPanel buttonPanel;
+   private IDField idField;
+   private PositionField xField;
+   private PositionField yField;
 
    /**
     * Preparing Frame with environment to shown
-    * @param width environment width
-    * @param height environment height
+    * 
+    * @param width
+    *           environment width
+    * @param height
+    *           environment height
     */
    public ActionFrame(int width, int height)
    {
       // Preparing frame for showing environment
       setTitle("Agents Environment");
-      setSize(width + 50, height + 100);
+      setSize(width + 50, height + 150);
       setResizable(false);
 
       // Adding panel with agents and objects positions
@@ -40,7 +50,7 @@ public class ActionFrame extends JFrame
       panel.setBorder(BorderFactory.createEtchedBorder());
 
       JPanel pane = new JPanel(); // panel, which sets environment not resize
-                                    // able
+      // able
       pane.setLayout(null);
       panel.setBounds(0, 0, width, height);
 
@@ -56,9 +66,58 @@ public class ActionFrame extends JFrame
       content.add(pan, BorderLayout.CENTER);
 
       // Adding panel with buttons
-      buttonPanel = new JPanel();
-      JButton koniec = new JButton("Zakoñcz");
-      buttonPanel.add(koniec);
+      JPanel buttonPanel = new JPanel();
+      buttonPanel.setLayout(new BorderLayout());
+
+      // adding new environment object section
+      idField = new IDField(5);
+      xField = new PositionField(4, width);
+      yField = new PositionField(4, height);
+      JLabel etyId = new JLabel("ID:");
+      JLabel etyX = new JLabel("X position:");
+      JLabel etyY = new JLabel("Y position:");
+
+      // adding section border
+      JPanel newObjectPanel = new JPanel();
+      Border bord = BorderFactory.createEtchedBorder();
+      newObjectPanel.setBorder(BorderFactory.createTitledBorder(bord,
+            "New environment object"));
+
+      newObjectPanel.add(etyId);
+      newObjectPanel.add(idField);
+      newObjectPanel.add(etyX);
+      newObjectPanel.add(xField);
+      newObjectPanel.add(etyY);
+      newObjectPanel.add(yField);
+
+      // adding Add button to section
+      JButton addObj = new JButton("Add");
+      newObjectPanel.add(addObj);
+      addObj.addActionListener(new ActionListener()
+      {
+         public void actionPerformed(ActionEvent zd)
+         {
+            Integer id = idField.pobierzWartosc();
+            Integer x = xField.pobierzWartosc();
+            Integer y = yField.pobierzWartosc();
+            
+            if (id != null && x != null && y != null)
+            {
+               EnvObject newObj = new EnvObject(id, x, y);
+               panel.addEnvObject(newObj);
+               repaint();
+            }
+            // tu wywolana byc powinna funkcja dodajca obiekt od env
+         }
+      });
+
+      buttonPanel.add(newObjectPanel, BorderLayout.NORTH);
+
+      // Close button
+      JButton koniec = new JButton("Close");
+      JPanel bPanel = new JPanel();
+      bPanel.add(koniec);
+      buttonPanel.add(bPanel);
       koniec.addActionListener(new ActionListener()
       {
          public void actionPerformed(ActionEvent zd)
@@ -73,115 +132,190 @@ public class ActionFrame extends JFrame
    /**
     * Upgrades data which will be shown in environment
     * 
-    * @param newObjects new objects list
-    * @param newAgents new agents list
+    * @param newObjects
+    *           new objects list
+    * @param newAgents
+    *           new agents list
     */
    public void upgrade(ArrayList<EnvObject> newObjects,
          ArrayList<EnvObject> newAgents)
    {
       panel.upgrade(newObjects, newAgents);
    }
-}
 
-/**
- * Class containing graphic representation of environment.
- * 
- * @author Maciek
- */
-class EnvPanel extends JPanel
-{
-   private static double PWIDTH = 4;
-   private static double PHIGHT = 4;
-
-   private ArrayList<EnvObject> envObjects;
-   private ArrayList<EnvObject> agents;
-
-   /**
-    * Creates panel with empty object and agent lists
-    */
-   public EnvPanel()
+   public void reDraw()
    {
-      envObjects = new ArrayList<EnvObject>();
-      agents = new ArrayList<EnvObject>();
+      repaint();
    }
 
+   // *********************************************************************
    /**
-    * Creates panel with given objects and agents lists
-    * @param newObjects objects list
-    * @param newAgents agents list
-    */
-   public EnvPanel(ArrayList<EnvObject> newObjects,
-         ArrayList<EnvObject> newAgents)
-   {
-      envObjects = newObjects;
-      agents = newAgents;
-   }
-
-   /**
-    * Method used to paint component, used automatically when repaint method
-    * used.
-    */
-   public void paintComponent(Graphics g)
-   {
-      super.paintComponents(g);
-      Graphics2D g2 = (Graphics2D) g;
-      Font arial5 = new Font("Arial", Font.BOLD, 10);
-      g2.setFont(arial5);
-
-      // painting all objects
-      Iterator<EnvObject> iter = envObjects.iterator();
-
-      while (iter.hasNext())
-      {
-         EnvObject object = iter.next();
-
-         int centX = object.getPosX();
-         int centY = object.getPosY();
-
-         Ellipse2D circle = new Ellipse2D.Double(centX - PWIDTH / 2, centY
-               - PHIGHT / 2, PWIDTH, PHIGHT);
-         g2.setPaint(Color.RED);
-         g2.fill(circle);
-         g2.draw(circle);
-         g2
-               .drawString(new Integer(object.getId()).toString(), centX + 5,
-                     centY);
-      }
-
-      // painting all agents
-      iter = agents.iterator();
-
-      while (iter.hasNext())
-      {
-         EnvObject object = iter.next();
-
-         int centX = object.getPosX();
-         int centY = object.getPosY();
-
-         Rectangle2D rect = new Rectangle2D.Double(centX - PWIDTH / 2, centY
-               - PHIGHT / 2, PWIDTH, PHIGHT); //preparing agent symbol
-         Ellipse2D circle = new Ellipse2D.Double(centX - 10 * PWIDTH / 2, centY
-               - 10 * PHIGHT / 2, 10 * PWIDTH, 10 * PHIGHT); // painting signal coverage
-         g2.setPaint(Color.BLUE);
-         g2.fill(rect);
-         g2.draw(rect);
-         g2.draw(circle);
-         g2
-               .drawString(new Integer(object.getId()).toString(), centX + 5,
-                     centY); //writing agent id
-      }
-   }
-
-   /**
-    * Upgrades data which will be shown in panel
+    * Class containing graphic representation of environment.
     * 
-    * @param newObjects new objects list
-    * @param newAgents new agents list
+    * @author Maciek
     */
-   public void upgrade(ArrayList<EnvObject> newObjects,
-         ArrayList<EnvObject> newAgents)
+   class EnvPanel extends JPanel
    {
-      envObjects = newObjects;
-      agents = newAgents;
+      private static final double PWIDTH = 4;
+      private static final double PHIGHT = 4;
+
+      private ArrayList<EnvObject> envObjects;
+      private ArrayList<EnvObject> agents;
+      private EnvObject actual;
+
+      /**
+       * Creates panel with empty object and agent lists
+       */
+      public EnvPanel()
+      {
+         envObjects = new ArrayList<EnvObject>();
+         agents = new ArrayList<EnvObject>();
+
+         actual = null;
+
+         addMouseMotionListener(new ObjectMoveListener());
+         addMouseListener(new ObjectClickedListener());
+      }
+
+      /**
+       * Creates panel with given objects and agents lists
+       * 
+       * @param newObjects
+       *           objects list
+       * @param newAgents
+       *           agents list
+       */
+      public EnvPanel(ArrayList<EnvObject> newObjects,
+            ArrayList<EnvObject> newAgents)
+      {
+         envObjects = newObjects;
+         agents = newAgents;
+
+         actual = null;
+
+         addMouseMotionListener(new ObjectMoveListener());
+         addMouseListener(new ObjectClickedListener());
+      }
+
+      /**
+       * Method used to paint component, used automatically when repaint method
+       * used.
+       */
+      public void paintComponent(Graphics g)
+      {
+         super.paintComponents(g);
+         Graphics2D g2 = (Graphics2D) g;
+         Font arial5 = new Font("Arial", Font.BOLD, 10);
+         g2.setFont(arial5);
+
+         // painting all objects
+         Iterator<EnvObject> iter = envObjects.iterator();
+
+         while (iter.hasNext())
+         {
+            EnvObject object = iter.next();
+
+            int centX = object.getPosX();
+            int centY = object.getPosY();
+
+            Ellipse2D circle = new Ellipse2D.Double(centX - PWIDTH / 2, centY
+                  - PHIGHT / 2, PWIDTH, PHIGHT);
+            g2.setPaint(Color.RED);
+            g2.fill(circle);
+            g2.draw(circle);
+            g2.drawString(new Integer(object.getId()).toString(), centX + 5,
+                  centY);
+         }
+
+         // painting all agents
+         iter = agents.iterator();
+
+         while (iter.hasNext())
+         {
+            EnvObject object = iter.next();
+
+            int centX = object.getPosX();
+            int centY = object.getPosY();
+
+            Rectangle2D rect = new Rectangle2D.Double(centX - PWIDTH / 2, centY
+                  - PHIGHT / 2, PWIDTH, PHIGHT); // preparing agent symbol
+            Ellipse2D circle = new Ellipse2D.Double(centX - 10 * PWIDTH / 2,
+                  centY - 10 * PHIGHT / 2, 10 * PWIDTH, 10 * PHIGHT); // painting
+            // signal
+            // coverage
+            g2.setPaint(Color.BLUE);
+            g2.fill(rect);
+            g2.draw(rect);
+            g2.draw(circle);
+            g2.drawString(new Integer(object.getId()).toString(), centX + 5,
+                  centY); // writing agent id
+         }
+      }
+
+      /**
+       * Upgrades data which will be shown in panel
+       * 
+       * @param newObjects
+       *           new objects list
+       * @param newAgents
+       *           new agents list
+       */
+      public void upgrade(ArrayList<EnvObject> newObjects,
+            ArrayList<EnvObject> newAgents)
+      {
+         envObjects = newObjects;
+         agents = newAgents;
+      }
+
+      public void addEnvObject(EnvObject newObj)
+      {
+         envObjects.add(newObj);
+      }
+
+      public EnvObject find(Point2D p)
+      {
+         for (int i = 0; i < envObjects.size(); i++)
+         {
+            EnvObject o = envObjects.get(i);
+
+            int centX = o.getPosX();
+            int centY = o.getPosY();
+
+            Rectangle2D r = new Rectangle2D.Double(centX - PWIDTH / 2, centY
+                  - PHIGHT / 2, PWIDTH, PHIGHT);
+            if (r.contains(p))
+               return o;
+         }
+         return null;
+      }
+
+      private class ObjectClickedListener extends MouseAdapter
+      {
+         public void mousePressed(MouseEvent event)
+         {
+            actual = find(event.getPoint());
+         }
+      }
+
+      private class ObjectMoveListener implements MouseMotionListener
+      {
+         public void mouseDragged(MouseEvent event)
+         {
+            if (actual != null)
+            {
+               actual.setPosX(event.getX());
+               actual.setPosY(event.getY());
+               reDraw();
+            }
+         }
+
+         public void mouseMoved(MouseEvent event)
+         {
+            if (find(event.getPoint()) == null)
+               setCursor(Cursor.getDefaultCursor());
+            else
+               setCursor(Cursor.getPredefinedCursor(Cursor.MOVE_CURSOR));
+         }
+      }
    }
 }
---
+++ trunk/robotagents/src/environment/IDField.java
@@ -0,0 +1,127 @@
+package environment;
+
+import javax.swing.InputVerifier;
+import javax.swing.JComponent;
+import javax.swing.JTextField;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+import javax.swing.text.JTextComponent;
+import javax.swing.text.PlainDocument;
+
+/**
+ * Text field to get new objectID
+ * @author Maciej Kupczak
+ *
+ */
+public class IDField extends JTextField
+{
+   /**
+    * Creates 
+    * @param domwar domyœlna wartoœc, jezeli d³u¿sza ni¿ 4 czyfry to przycinana do 4 pierwszych
+    * @param rozmiar rozmiar pola tekstowego
+    */
+   public IDField(int size)
+   {
+      super(size);
+      setInputVerifier(new WerLength(size));
+   }
+   
+   protected Document createDefaultModel()
+   {
+      return new IdDoc();
+   }
+   /**
+    * sprawdza czy zawartosc pola jest poprawna liczba calkowita czterocyfrowa
+    * @return true jesli jest poprawna
+    */
+   public boolean jestPoprawny()
+   {
+      return IdDoc.poprawny(getText());
+   }
+   /**
+    * Pobiera wartosc liczbowa pola tekstowwego
+    * @return liczba wpisana w polu, 1337 jesli zawartosc jest nie poprawna
+    */
+   public Integer pobierzWartosc()
+   {
+      try
+      {
+         return Integer.parseInt(getText());
+      }
+      catch(NumberFormatException w)
+      {
+         return null;
+      }
+   }
+}
+
+/**
+ * Weryfikator, który sprawdza, czy zawartosc pola tekstowego jest poprawna liczb¹ ca³kowit¹
+ * @author Maciek
+ *
+ */
+class WerLength extends InputVerifier
+{
+   private int size;
+   
+   public WerLength(int s)
+   {
+      size = s;
+   }
+   
+   public boolean verify(JComponent komponent)
+   {
+      String tekst = ((JTextComponent)komponent).getText();
+      if (tekst.length()>size)
+         return false;
+      else
+         return IdDoc.poprawny(tekst);     
+   }
+}
+
+/**
+ * Dokument który moze przechowywa tylko 4 cyfrowe integery
+ * @author Maciej Kupczak
+ */
+class IdDoc extends PlainDocument
+{
+   
+   public void insertString(int poz, String str, AttributeSet a) throws BadLocationException
+   {
+      if (str==null) return;
+      
+      String dawnyString = getText(0, getLength());
+      String nowyString = dawnyString.substring(0,poz)+str+dawnyString.substring(poz);
+      
+      if (poprawny(nowyString))
+      {  
+         if ((nowyString.length())>4)
+         {
+            nowyString = nowyString.substring(0, 4);
+            super.replace(0, 4, nowyString, a);
+         }
+         else 
+         {
+            super.insertString(poz, str, a);
+         }
+      }
+   }
+   /**
+    * sprawdza czy podany string jest liczb¹
+    * @param str sprawdzany ci¹g
+    * @return true jeœli jest liczba, false w przeciwnym wypadku
+    */
+   static boolean poprawny(String str)
+   {
+      try
+      {
+         Integer.parseInt(str);
+         return true;
+      }
+      catch(NumberFormatException w)
+      {
+         return false;
+      }
+   }
+}
